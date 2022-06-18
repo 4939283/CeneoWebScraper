e34ecb89fc6c884dc39195954346e12d01e356b1
4939283 <s223051@student.uek.krakow.pl>
2022-06-18 19:01:43 +0200

    Completed all features and bootstrap

--- app/models/product.py
+++ app/models/product.py
@@ -1,27 +1,93 @@
-import requests
 import os
 import json
-import pandas as pd
+import requests
 import numpy as np
+import pandas as pd
 from bs4 import BeautifulSoup
 from app.utils import get_item
 from app.models.opinion import Opinion
+
+import matplotlib
+matplotlib.use('Agg')
 from matplotlib import pyplot as plt
-from numpyencoder import NumpyEncoder
 
-plt.switch_backend('Agg') 
-
-class Product:
-    def __init__(self, product_id=0, opinions=[], product_name="", opinions_count=0, pros_count=0, cons_count=0, average_score=0):
+class Product():
+    def __init__(self, product_id, product_name="", opinions=[], opinions_count=0, pros_count=0, cons_count=0, average_score=0):
         self.product_id = product_id
         self.product_name = product_name
-        self.opinions = opinions
+        self.opinions = opinions[:]
         self.opinions_count = opinions_count
         self.pros_count = pros_count
         self.cons_count = cons_count
         self.average_score = average_score
+
+    def extract_name(self):
+        url = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
+        response = requests.get(url)
+        page = BeautifulSoup(response.text, "html.parser")
+        self.product_name = get_item(page, "h1.product-top__product-info__name")
+        return self
+
+    def extract_opinions(self):
+        url = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
+        while(url):
+            response = requests.get(url)
+            page = BeautifulSoup(response.text, "html.parser")
+            opinions = page.select("div.js_product-review")
+            for opinion in opinions:
+                single_opinion = Opinion().extract_opinion(opinion)
+                self.opinions.append(single_opinion)
+            try:
+                url = "https://www.ceneo.pl" + get_item(page, "a.pagination__next", "href")
+            except TypeError:
+                url = None
+        return self
     
-    def __str__(self):
+    def opinions_to_df(self):
+        opinions = pd.read_json(json.dumps(self.opinions_to_dict()))
+        opinions["stars"] = opinions["stars"].map(lambda x: float(x.split("/")[0].replace(",", ".")))
+        return opinions
+
+    def calculate_stats(self):
+        opinions = self.opinions_to_df()
+        self.opinions_count = len(opinions)
+        self.pros_count = int(opinions["pros"].map(bool).sum())
+        self.cons_count = int(opinions["cons"].map(bool).sum())
+        self.average_score = opinions["stars"].mean().round(2)
+        return self
+    
+    def draw_charts(self):
+        opinions = self.opinions_to_df()
+        if not os.path.exists("app/static/plots"):
+            os.makedirs("app/static/plots")
+        recommendation = opinions["recommendation"].value_counts(dropna=False).sort_index().reindex(["Nie polecam", "Polecam", None], fill_value=0)
+        try:
+            recommendation.plot.pie(
+                label = "",
+                autopct = lambda p: "{:.1f}%".format(round(p)) if p > 0 else "",
+                colors = ["crimson", "forestgreen", "lightskyblue"],
+                labels = ["Nie polecam", "Polecam", "Nie mam zdania"],
+                normalize = True
+            )
+        except ValueError:
+            pass
+        plt.title("Rekomendacje")
+        plt.savefig(f"app/static/plots/{self.product_id}_recommendations.png")
+        plt.close()
+        stars = opinions["stars"].value_counts().sort_index().reindex(list(np.arange(0, 5.5, 0.5)), fill_value=0)
+        stars.plot.bar(
+            color = "pink"
+        )
+        plt.title("Oceny produktu")
+        plt.xlabel("Liczba gwiazdek")
+        plt.ylabel("Liczba opinii")
+        plt.grid(True, axis="y")
+        plt.xticks(rotation=0)
+        plt.savefig(f"app/static/plots/{self.product_id}_stars.png")
+        plt.close()
+        return self
+
+    def __str__(self) -> str:
         return f"""product_id: {self.product_id}<br>
         product_name: {self.product_name}<br>
         opinions_count: {self.opinions_count}<br>
@@ -31,10 +97,10 @@ class Product:
         opinions: <br><br>
         """ + "<br><br>".join(str(opinion) for opinion in self.opinions)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Product(product_id={self.product_id}, product_name={self.product_name}, opinions_count={self.opinions_count}, pros_count={self.pros_count}, cons_count={self.cons_count}, average_score={self.average_score}, opinions: [" + ", ".join(opinion.__repr__() for opinion in self.opinions) + "])"
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return {
             "product_id": self.product_id,
             "product_name": self.product_name,
@@ -44,7 +110,7 @@ class Product:
             "average_score": self.average_score,
             "opinions": [opinion.to_dict() for opinion in self.opinions]
         }
-
+    
     def stats_to_dict(self):
         return {
             "product_id": self.product_id,
@@ -52,86 +118,36 @@ class Product:
             "opinions_count": self.opinions_count,
             "pros_count": self.pros_count,
             "cons_count": self.cons_count,
-            "average_score": self.average_score
+            "average_score": self.average_score,
         }
     
     def opinions_to_dict(self):
         return [opinion.to_dict() for opinion in self.opinions]
 
-    def extract_product(self):
-        url = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
-        response = requests.get(url)
-        page = BeautifulSoup(response.text, 'html.parser')
-        self.product_name = get_item(page, "h1.product-top__product-info__name")
-        while(url):
-            response = requests.get(url)
-            page = BeautifulSoup(response.text, 'html.parser')
-            opinions = page.select("div.js_product-review")
-            for opinion in opinions:
-                self.opinions.append(Opinion().extract_opinion(opinion))
-            try:    
-                url = "https://www.ceneo.pl"+get_item(page,"a.pagination__next","href")
-            except TypeError:
-                url = None
-        return self
-    
-    def opinions_to_df(self):
-        opinions = pd.read_json(json.dumps([opinion.to_dict() for opinion in self.opinions]))
-        opinions.stars = opinions.stars.map(lambda x: float(x.split("/")[0].replace(",", ".")))
-        return opinions
-    
-    def process_stats(self):
-        self.opinions_count = self.opinions_to_df().shape[0]
-        self.pros_count = self.opinions_to_df().pros.map(bool).sum()
-        self.cons_count = self.opinions_to_df().cons.map(bool).sum()
-        self.average_score = self.opinions_to_df().stars.mean().round(2)
-        return self
-
-    def draw_charts(self): 
-        recommendation = self.opinions_to_df().recommendation.value_counts(dropna = False).sort_index().reindex(["Nie polecam", "Polecam", None])
-        recommendation.plot.pie(
-            label="", 
-            autopct="%1.1f%%", 
-            colors=["lightsalmon", "lightgreen", "lightskyblue"],
-            labels=["Nie polecam", "Polecam", "Nie mam zdania"]
-        )
-        plt.title("Rekomendacja")
-        plt.savefig(f"app/static/plots/{self.product_id}_recommendations.png")
-        plt.close()
-        stars = self.opinions_to_df().stars.value_counts().sort_index().reindex([x / 10.0 for x in range(5, 50, 5)], fill_value=0)
-        stars.plot.bar()
-        plt.title("Oceny produktu")
-        plt.xlabel("Liczba gwiazdek")
-        plt.ylabel("Liczba opinii")
-        plt.grid(True)
-        plt.xticks(rotation=0)
-        plt.savefig(f"app/static/plots/{self.product_id}_stars.png")
-        plt.close()
-
-    def save_opinions(self):        
+    def export_opinions(self):
         if not os.path.exists("app/opinions"):
             os.makedirs("app/opinions")
         with open(f"app/opinions/{self.product_id}.json", "w", encoding="UTF-8") as jf:
             json.dump(self.opinions_to_dict(), jf, indent=4, ensure_ascii=False)
-    
-    def save_stats(self):        
+
+    def export_product(self):
         if not os.path.exists("app/products"):
             os.makedirs("app/products")
-        with open(f"app/products/{self.product_id}.json", "w", encoding="UTF-8") as jf:
-            json.dump(self.stats_to_dict(), jf, indent=4,sort_keys=True,
-              separators=(', ', ': '), ensure_ascii=False,
-              cls=NumpyEncoder)
-              
-    def read_from_json(self):
-        with open(f"app/products/{self.product_id}.json", "r", encoding="UTF-8") as jf:
-            product = json.load(jf)
-        self.product_id = product["product_id"]
-        self.product_name = product["product_name"]
-        self.opinions_count = product["opinions_count"]
-        self.pros_count = product["pros_count"]
-        self.cons_count = product["cons_count"]
-        self.average_score = product["average_score"]
-        with open(f"app/opinions/{self.product_id}.json", "r", encoding="UTF-8") as jf:
-            opinions = json.load(jf)
-        for opinion in opinions:
-            self.opinions.append(Opinion(**opinion))
+        with open(f"app/products/{self.product_id}.json", "w", encoding="UTF-8") as jfile:
+            json.dump(self.stats_to_dict(), jfile, indent=4, ensure_ascii=False)
+    
+    def import_product(self):
+        if os.path.exists(f"app/products/{self.product_id}.json"):
+            with open(f"app/products/{self.product_id}.json", "r", encoding="UTF-8") as jfile:
+                product = json.load(jfile)
+                self.product_id = product["product_id"]
+                self.product_name = product["product_name"]
+                self.opinions_count = product["opinions_count"]
+                self.pros_count = product["pros_count"]
+                self.cons_count = product["cons_count"]
+                self.average_score = product["average_score"]
+        if os.path.exists(f"app/opinions/{self.product_id}.json"):
+            with open(f"app/opinions/{self.product_id}.json", "r", encoding="UTF-8") as jfile:
+                opinions = json.load(jfile)
+                for opinion in opinions:
+                    self.opinions.append(Opinion(**opinion))
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, send_file
 import os
 from app.models.product import Product
 
@@ -12,16 +12,16 @@ def extract():
     if request.method == "POST":
         product_id = request.form.get("product_id")
         product = Product(product_id)
-        product.extract_product()
-
+        product.extract_name()
         if product.product_name == None:
             error = True
             return render_template("extract.html.jinja", error=error)
         else:
-            product.process_stats()
+            product.extract_opinions()
+            product.calculate_stats()
             product.draw_charts()
-            product.save_opinions()
-            product.save_stats()
+            product.export_opinions()
+            product.export_product()
             return redirect(url_for("product", product_id=product_id))
     else:
         return render_template("extract.html.jinja")
@@ -31,7 +31,7 @@ def products():
     products = [filename.split(".")[0] for filename in os.listdir("app/opinions")]
     for i in range(0, len(products)):
         products[i] = Product(products[i])
-        products[i].read_from_json()
+        products[i].import_product()
     return render_template("products.html.jinja", products=products)
 
 @app.route('/author')
@@ -39,9 +39,20 @@ def author():
     return render_template("author.html.jinja")
 
 @app.route('/product/<product_id>')
-def product(product_id): 
+def product(product_id):
     product = Product(product_id)
-    product.read_from_json()
-    opinions = product.opinions_to_df()
+    product.import_product()
+    opinions = product.opinions_to_dict()
     stats = product.stats_to_dict()
-    return render_template("product.html.jinja", stats=stats, product_id=product_id, opinions_tables=[opinions.to_html(classes='data')], opinions_titles=opinions.columns.values)
+    return render_template("product.html.jinja", stats=stats, product_id=product_id, opinions=opinions)
+    
+@app.route('/charts/<product_id>')
+def charts(product_id):
+    return render_template("charts.html.jinja", product_id=product_id)
+
+@app.route('/download_json/<product_id>')
+def download_json(product_id):
+    return send_file(f"opinions\{product_id}.json",
+                     mimetype='text/json',
+                     attachment_filename=f'{product_id}.json',
+                     as_attachment=True)
